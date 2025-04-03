613ceb13aadfe97eee1e443d0d0a989aa627369f
Omsai Alladwar <omsaialladwar@gmail.com>
2025-04-03 09:02:08 +0530

    making changes for deployment

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,1 +1,3 @@
-export const BACKEND_URL = "http://localhost:4001/api/v1"; // Adjust the port if necessary
+// export const BACKEND_URL = "http://localhost:4001/api/v1";  only for localhost
+
+export const BACKEND_URL = "https://courseapp-ubep.onrender.com"; //for droployment  
